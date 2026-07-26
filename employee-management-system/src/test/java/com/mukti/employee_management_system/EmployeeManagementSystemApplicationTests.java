d0f7788ded4747dd9bcef4bed9650d7f80e16c14
MuktiKumari983 <kumarimukti789@gmail.com>
2026-07-26 15:39:27 +0530

    Initial Spring Boot project setup

---
+++ employee-management-system/src/test/java/com/mukti/employee_management_system/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukti.employee_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
